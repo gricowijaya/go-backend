65e77492bbebfafc9cd8b2e11c3ce3af90040497
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-12-06 19:00:20 +0800

    add migrations and database docker configuration

---
+++ user-model-db/db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,20 @@
+-- SQL dump generated using DBML (dbml-lang.org)
+-- Database: PostgreSQL
+-- Generated at: 2022-12-06T08:48:33.842Z
+
+CREATE TABLE "Users" (
+  "id" integer PRIMARY KEY,
+  "email" varchar(50),
+  "username" varchar(12),
+  "password" varchar(20)
+);
+
+CREATE TABLE "UserDetails" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer,
+  "fullname" varchar(50),
+  "phone" varchar(13),
+  "gender" varchar(20)
+);
+
+ALTER TABLE "UserDetails" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("id");
